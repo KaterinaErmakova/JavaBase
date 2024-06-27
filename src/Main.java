cd13e2e2f0e285afb962263941841181f0314acc
Katerina_Ermakova <katrin6793@gmail.com>
2024-06-27 20:56:32 +0300

    Добавлено решение задачи с выводом нечётных чисел

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        RandomNumber.startGame();
-        System.out.println("Программа завершена");
+//        RandomNumber.startGame();
+//        System.out.println("Программа завершена");
+        PrintNumbersFromInterval.start();
     }
 }
