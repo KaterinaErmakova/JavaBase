843401ba7ed5c59704a895971cd7553659b0450e
Katerina_Ermakova <katrin6793@gmail.com>
2024-07-26 01:47:32 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,2 @@
-public class Main {
-    public static void main(String[] args) {
-//        RandomNumber.startGame();
-//        System.out.println("Программа завершена");
-        PrintNumbersFromInterval.start();
-    }
+package PACKAGE_NAME;public class Main {
 }
